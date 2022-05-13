4ccbbee368159737aa5a0ee8977e668d5d2512d7
Ranjitha9945 <ranjithagowda3597@gmail.com>
2022-05-13 15:45:53 +0530

    4th commit

---
+++ src/test/java/crm/sampleframe/SampleClassTest3.java
@@ -0,0 +1,13 @@
+package crm.sampleframe;
+
+import org.testng.annotations.Test;
+
+public class SampleClassTest3 
+{
+	@Test
+	public void createContact()
+	{
+		System.out.println("execute createContact3");
+	}
+
+}
