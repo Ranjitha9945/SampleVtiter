07bceb5646d0192264452b44aaa055d7829a0dcd
Ranjitha9945 <ranjithagowda3597@gmail.com>
2022-05-13 15:45:36 +0530

    3rd commit

--- src/test/java/crm/sampleframe/SampleClassTest.java
+++ src/test/java/crm/sampleframe/SampleClassTest.java
@@ -7,7 +7,7 @@ public class SampleClassTest
 	@Test
 	public void createContact()
 	{
-		System.out.println("execute createContact2");
+		System.out.println("execute createContact3");
 		
 		System.out.println("********url from cmd line*********"+System.getProperty("url"));
 		System.out.println("********browser from eclipse*********"+System.getProperty("browser"));
