616cdc17f05270734f345f2da19993e11677f451
Ranjitha9945 <ranjithagowda3597@gmail.com>
2022-05-13 12:41:33 +0530

    1st commit

---
+++ src/test/java/crm/sampleframe/SampleClassTest.java
@@ -0,0 +1,16 @@
+package crm.sampleframe;
+
+import org.testng.annotations.Test;
+
+public class SampleClassTest 
+{
+	@Test
+	public void createContact()
+	{
+		System.out.println("execute createContact");
+		
+		System.out.println("********url from cmd line*********"+System.getProperty("url"));
+		System.out.println("********browser from eclipse*********"+System.getProperty("browser"));
+	}
+
+}
