26a4eb4939c7fb698d08ea4f780f51fb1efa4393
Ranjitha9945 <ranjithagowda3597@gmail.com>
2022-05-13 15:19:56 +0530

    2nd commit

--- src/test/java/crm/sampleframe/SampleClassTest2.java
+++ src/test/java/crm/sampleframe/SampleClassTest2.java
@@ -2,12 +2,12 @@ package crm.sampleframe;
 
 import org.testng.annotations.Test;
 
-public class SampleClassTest
+public class SampleClassTest2 
 {
 	@Test
 	public void createContact()
 	{
-		System.out.println("execute createContact2");
+		System.out.println("execute createContact");
 		
 		System.out.println("********url from cmd line*********"+System.getProperty("url"));
 		System.out.println("********browser from eclipse*********"+System.getProperty("browser"));
